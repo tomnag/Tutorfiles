eb97c8cbf434dfc8d9e4b346c461296cd199efd4
tomnag <43217431+tomnag@users.noreply.github.com>
2020-12-18 00:57:19 -0500

    Add files via upload

---
+++ LynnetteDragDrop/HTML/Assets/spanishLanguagePack.js
@@ -0,0 +1,23 @@
+var CTATLanguagePack =
+{
+        "HORIZONTAL" : "LR", // Accepted values are: LR, RL
+        "VERTICAL" : "TB",      // Accepted values are TB, BT
+        "LOADING" : "Por favor espera mientras se carga el tutor",
+        "NEXTPROBLEM" : "Obteniendo el siguiente problema...",
+        "CONGRATULATIONS_YOU_ARE_DONE": "Felicidades, has terminado con este problema.",
+        "SURE_YOU_ARE_DONE": "Are you sure you are done?",
+        "TUTORDISCONNECTED" : "The tutor has disconnected. Please refresh the page.",
+        "OUTOFORDER" : "You need to do other steps first, before doing the step you just worked on. You might request a hint for more help.",
+        "DONE" : "Terminar Problema",
+        "HINT" : "Ayuda",
+        "NEXT" : "Siquiente",
+        "PREVIOUS" : "Anterior",
+		"NO_HINT_AVAILABLE" : "No hay ayuda disponible en este paso",
+        "HIGHLIGHTEDSTEP" : "Instead of the step you are working on, please work on the highlighted step.",
+        "NOTDONE" : "Lo siento! Pero todavía no has terminado",
+        "AUTHORPLEASECLOSE" : "Authoring Tools disconnected. Please close this page.",
+        "ERROR_502" : "Error contacting the server, please refresh the page and try again (HTTP status 502: gateway response).",
+        "ERROR_CONN_TS" : "Connection refused by tutoring service, please check your server or contact an administrator.",
+        "ERROR_CONN_LS" : "Connection refused by the logging server, please check your connection or contact an administrator.",
+        "AWAITING_OTHER_COLLABORATORS": "Waiting for other team members..."
+};
